7fcf0abe8c79f399306adfd83c0418ad074f32de
Iman Mirazimi <imirazimi@gmail.com>
2025-11-29 02:56:01 +0330

    fix some bug

--- internal/infra/migration/mysql/001_create_todos.up.sql
+++ internal/infra/migration/mysql/001_create_todos.up.sql
@@ -1,5 +1,7 @@
+
 CREATE TABLE IF NOT EXISTS todos (
   id VARCHAR(64) PRIMARY KEY,
   description TEXT,
   due_date DATETIME
 );
+  
--- internal/todo/service/create.go
+++ internal/todo/service/create.go
@@ -15,12 +15,11 @@ func NewTodoService(repo port.Repository, redis port.RedisStreamPublisher) *Todo
 	return &TodoServiceImpl{repo: repo, redis: redis}
 }
 
-// TODO:
+// TODO: need to implement Outbox Pattern to ensure data consistency between MySQL and Redis.
 func (s *TodoServiceImpl) CreateTodo(ctx context.Context, item *todo.TodoItem) error {
 	if err := s.repo.Create(ctx, item); err != nil {
 		return err
 	}
-	//
 	if err := s.redis.PublishTodo(ctx, item); err != nil {
 		return err
 	}
